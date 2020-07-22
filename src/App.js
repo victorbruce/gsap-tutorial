444aa92df8c4b0990b07b80ce859d7c956139ce3
victorbruce <victorbruce82@gmail.com>
2020-07-22 21:42:38 +0000

    feat: gsap tween max example 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 // 3rd-party libraries
-import gsap from "gsap";
-
-// assets
-import logo from "./logo.svg";
+import { TweenMax, Power3 } from "gsap";
 
 function App() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  let app = useRef(null);
+  let circleRed = useRef(null);
+  let circleYellow = useRef(null);
+  let circleGreen = useRef(null);
 
   useEffect(() => {
-    gsap.from(".App__description > small", 1, { opacity: 0, y: 50 });
-
-    gsap.from(".App__image", 1, { opacity: 0, scale: 0.8, delay: 0.8 });
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+    TweenMax.from(circleRed, 0.8, { opacity: 0, x: 40, ease: Power3.easeOut });
+    TweenMax.from(circleYellow, 0.8, {
+      opacity: 0,
+      x: 60,
+      ease: Power3.easeOut,
+      delay: 0.2,
+    });
+    TweenMax.from(circleGreen, 0.8, {
+      opacity: 0,
+      x: 80,
+      ease: Power3.easeOut,
+      delay: 0.2,
+    });
   }, []);
-
   return (
-    <div className="App">
-      <div className="App__inner">
-        <div className="App__image">
-          <img src={logo} alt="react-icon" />
-        </div>
-        <div className="App__description">
-          <small>React + Gsap</small>
-        </div>
+    <div className="App" ref={(el) => (app = el)}>
+      <h1>Gsap TweenMax</h1>
+
+      <div className="Circle__container">
+        <div className="Circle__color red" ref={(el) => (circleRed = el)}></div>
+        <div
+          className="Circle__color yellow"
+          ref={(el) => (circleYellow = el)}
+        ></div>
+        <div
+          className="Circle__color green"
+          ref={(el) => (circleGreen = el)}
+        ></div>
       </div>
     </div>
   );
