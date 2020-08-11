f21a608aa185fdc33fc4bf2200bb73023ed47da7
victorbruce <victorbruce82@gmail.com>
2020-08-11 05:50:26 +0000

    fix: intersection observer for multiple elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React, { useRef } from "react";
+import React, { useEffect } from "react";
 import gsap from "gsap";
-import { useIntersection } from "react-use";
+// import { useIntersection } from "react-use";
 
 const App = () => {
-  const sectionRef = useRef(null);
-
-  const intersection = useIntersection(sectionRef, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.9,
-  });
-
   const fadeIn = (element) => {
     gsap.to(element, 1, {
       opacity: 1,
@@ -24,37 +16,63 @@ const App = () => {
     gsap.to(element, 1, { opacity: 0, y: -20, ease: "power4.out" });
   };
 
-  intersection && intersection.intersectionRatio < 0.9
-    ? // Not reached
-      fadeOut(".fadeIn")
-    : // Reached so animate
-      fadeIn(".fadeIn");
+  function fadeInAnim(config) {
+    const observer = new IntersectionObserver((entries) => {
+      console.log(entries);
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio < 0.4) {
+          console.log("in the view");
+          fadeOut(entry.target);
+          // observer.unobserve(entry.target);
+        } else {
+          console.log("out of view");
+          fadeIn(entry.target);
+        }
+      });
+    }, config);
+
+    return observer;
+  }
+
+  const config = {
+    rootMargin: "50px",
+    threshold: 0.4,
+  };
+
+  useEffect(() => {
+    const sections = document.querySelectorAll(".fadeIn");
+    const observer = fadeInAnim(config);
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  });
 
   return (
     <div className="App">
-      <div className="App__inner">
-        <section className="App__about">
-          <div className="App__about__content">
-            <h2>About</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </section>
-        <section ref={sectionRef} className="App__testimonials">
+      <section className="App__about">
+        <div className="App__content">
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </section>
+      <section className="App__testimonials">
+        <div className="App__content">
           <h2 className="fadeIn">Testimonials</h2>
           <p className="fadeIn">
             {" "}
@@ -73,8 +91,10 @@ const App = () => {
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
-        </section>
-        <section className="App__blog">
+        </div>
+      </section>
+      <section className="App__blog">
+        <div className="App__content">
           <h2 className="fadeIn">Blog</h2>
           <p className="fadeIn">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -92,8 +112,8 @@ const App = () => {
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 };
